5f8a1b2526365e0a60e0b982fb98a4045be860ef
Nahid <nahid@glabbr.tech>
2025-05-12 20:04:59 +0530

    removed lint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // ... existing code ...
+  rules: {
+    'react-hooks/exhaustive-deps': 'off'  // This will disable the specific warning you're seeing
+  }
+  // ... existing code ...
+}
